72ef8ca04ee9d512e80ff7c189b5fb51ca182449
Cornell van der Straaten <30183@ma-web.nl>
2020-06-03 10:47:32 +0200

    fixed?

--- private/routes.php
+++ private/routes.php
@@ -12,8 +12,9 @@ SimpleRouter::group( [ 'prefix' => site_url() ], function () {
 	// Lees de docs, daar zie je hoe je routes kunt maken: https://github.com/skipperbent/simple-php-router#routes
 
 	SimpleRouter::get( '/', 'WebsiteController@home' )->name( 'home' );
-	// STOP: Tot hier al je eigen URL's zetten
 
+
+	// STOP: Tot hier al je eigen URL's zetten
 	SimpleRouter::get( '/not-found', function () {
 		http_response_code( 404 );
 
